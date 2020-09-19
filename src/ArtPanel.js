045da29d134dd401e795207da6f44397edbf763d
Kristen Andersen <kristen.andersen@gmail.com>
2020-09-19 02:34:40 -0500

    addl tile info styling

--- src/ArtPanel.js
+++ src/ArtPanel.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import Gallery from "./Gallery";
 import "./ArtPanel.css";
 
 class ArtPanel extends Component {
+  state = {
+    displayInfo: false,
+  };
+
+  toggleInfo = () => {
+    this.setState({ displayInfo: !this.state.displayInfo });
+  };
+
   render() {
     return (
       <div className="ArtPanel">
-        <Gallery results={this.props.results} />
+        <div>
+          <IconButton
+            aria-label={
+              this.state.displayInfo ? "hide all details" : "show all details"
+            }
+            onClick={this.toggleInfo}
+          >
+            {this.state.displayInfo ? <InfoIcon /> : <InfoOutlinedIcon />}
+          </IconButton>
+        </div>
+        <Gallery
+          results={this.props.results}
+          displayInfo={this.state.displayInfo}
+          toggleInfo={this.toggleInfo}
+        />
       </div>
     );
   }
--- src/Gallery.js
+++ src/Gallery.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import InfoIcon from "@material-ui/icons/Info";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import "./Gallery.css";
+import { capitalize } from "./utils/helpers.js";
 
 class Gallery extends Component {
   render() {
@@ -14,20 +20,55 @@ class Gallery extends Component {
               <img
                 className="gridListImg"
                 src={`${result.thumbnailUrl}/full/350,/0/default.jpg`}
-                alt={result.title}
+                alt={`${result.thumbnailUrl}. ${result.title}. ${result.date_start}. The Art Institute of Chicago.`}
               />
               <GridListTileBar
-                title={result.title}
-                titlePosition="bottom"
-                // actionIcon={
-                //   <IconButton
-                //     aria-label={`info about ${result.title}`}
-                //     className="icon"
-                //   >
-                //     <InfoIcon />
-                //   </IconButton>
-                // }
-              />
+                className="GridListTileBar"
+                data-place={result.place_of_origin}
+                style={{
+                  height: "100%",
+                }}
+                subtitle={
+                  <div className="tile-subtitle">
+                    <div className="tile-info">
+                      <p className="tile-details">
+                        {result.artist_title
+                          ? result.artist_title + " "
+                          : "Unknown Artist "}
+                        | {result.place_of_origin}
+                      </p>
+                      <p className="tile-title">{result.title}</p>
+                      <p className="tile-details">
+                        {capitalize(result.classification_title)}
+                        {result.style_title
+                          ? " | " + capitalize(result.style_title)
+                          : null}
+                      </p>
+                      <p className="tile-details">{result.date_start}</p>
+                    </div>
+                    <div className="tile-icons">
+                      <IconButton style={{ color: "white" }}>
+                        <FavoriteBorderIcon />
+                      </IconButton>
+                      <IconButton
+                        aria-label={
+                          this.props.displayInfo
+                            ? "hide tile details"
+                            : "show tile details"
+                        }
+                        style={{ color: "white" }}
+                        onClick={this.props.toggleInfo}
+                      >
+                        {this.props.displayInfo ? (
+                          <InfoIcon />
+                        ) : (
+                          <InfoOutlinedIcon />
+                        )}
+                      </IconButton>
+                    </div>
+                  </div>
+                }
+              ></GridListTileBar>
             </GridListTile>
           ))}
         </GridList>
