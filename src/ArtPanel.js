99f3282fd503e02abf95a18b4e45392bc9862367
Kristen Andersen <kristen.andersen@gmail.com>
2020-09-26 23:31:05 -0500

    ArtPanel header styling

--- src/ArtPanel.js
+++ src/ArtPanel.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
-import SearchIcon from "@material-ui/icons/Search";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import TextField from "@material-ui/core/TextField";
 import Gallery from "./Gallery";
 import "./ArtPanel.css";
 
@@ -52,40 +55,59 @@ class ArtPanel extends Component {
     return (
       <div className="ArtPanel">
         {/* nav will be hidden until this.state.results === true */}
-        <nav>
-          <div>
-            <IconButton
-              aria-label={this.state.showSearch ? "hide Search" : "show Search"}
-              onClick={this.toggleSearch}
-            >
-              {/* will be disabled until input fields validated */}
-              <SearchIcon />
-            </IconButton>
+        <div className="input-form">
+          <form className="root" noValidate autoComplete="off">
+            <div className="form-info">
+              <label>Search</label>
+              <IconButton
+                aria-label={
+                  this.state.showSearch ? "hide Search" : "show Search"
+                }
+                onClick={this.toggleSearch}
+              >
+                {/* will be disabled until input fields validated */}
+                {this.state.showSearch ? (
+                  <ExpandLessIcon />
+                ) : (
+                  <ExpandMoreIcon />
+                )}
+              </IconButton>
+            </div>
+            {this.state.showSearch && (
+              <div className="input-content">
+                <div className="input-fields">
+                  <div className="left-input">
+                    <TextField id="standard-basic" label="start year" />
+                  </div>
+                  <div className="right-input">
+                    <TextField id="standard-basic" label="end year" />
+                  </div>
+                </div>
+                <div className="submit-button">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={this.props.onSearchSubmit}
+                    // data-theme="dark"
+                    disabled={!this.props.mapLoaded}
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </div>
+            )}
+          </form>
+        </div>
+        <Divider />
+        <div className="gallery-header">
+          <div className="results-length">
+            {this.props.results.length} results
           </div>
-          <div>
-            <IconButton
-              aria-label={
-                this.state.toggleAllDetails
-                  ? "hide all details"
-                  : "show all details"
-              }
-              onClick={this.toggleAllDetails}
-            >
-              {this.state.showAllDetails ? <InfoIcon /> : <InfoOutlinedIcon />}
-            </IconButton>
+          <div className="toggle-all-details" onClick={this.toggleAllDetails}>
+            {this.state.showAllDetails
+              ? "Hide all details"
+              : "Show all details"}
           </div>
-        </nav>
-        <div className="inputForm">
-          <label>GET ART</label>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={this.props.onSearchSubmit}
-            // data-theme="dark"
-            disabled={!this.props.mapLoaded}
-          >
-            Submit
-          </Button>
         </div>
 
         <Gallery
