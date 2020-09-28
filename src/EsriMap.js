7c9a06f4965a48426e1dff1bc96758e3104e4a17
Kristen Andersen <kristen.andersen@gmail.com>
2020-09-27 21:10:15 -0500

    remove event listener with ea update, so they don't accumulate

--- src/EsriMap.js
+++ src/EsriMap.js
@@ -11,10 +11,9 @@ import "./EsriMap.css";
 //   return theme === "light" ? "gray" : "dark-gray";
 // }
 
-//All API, no sampleArtwork.
-
-//inside CDU, was getting overwritten each update. Elsewhere, wasn't accessible
+//inside CDU, were getting overwritten each update. Elsewhere, weren't accessible
 let highlight;
+let mapClickListener;
 
 class EsriMap extends React.Component {
   constructor(props) {
@@ -37,40 +36,33 @@ class EsriMap extends React.Component {
   componentDidUpdate(prevProps) {
     if (this.props.results !== prevProps.results) {
       this.props.selectedPlace && this.props.removeSelectedPlace();
+
+      //so click listeners don't accumulate with ea Update & run multiple times
+      if (mapClickListener) {
+        console.log("mapClickListener found");
+        mapClickListener.remove();
+      }
+
       if (this._view) {
         //to delay no results msg in ArtPanel from loading, until it's known if there are actually no results
         this.props.setMapResultsLoad(true);
-        //existing map points removed if next search = no results
-        //if (!this.props.results.length) {
+
+        //existing map points removed, if next search has results or not
         let layer = "";
         layer = this._view.map.layers.getItemAt(0);
-        // console.log(
-        //   "CDU results lyr b remove",
-        //   this._view.map.layers.items.length
-        // );
         if (layer) {
           this._view.map.remove(layer);
-          //layer = "";
         }
-        // console.log(
-        //   "CDU results lyr a remove",
-        //   this._view.map.layers.items.length
-        // );
-        //} else {
-        //let layer = "";
+
         setGraphics(this.props.results)
           .then((graphicsArr) => {
             return (layer = loadLayer(graphicsArr));
           })
           .then((layer) => {
             this._view.map.add(layer);
-            // console.log(
-            //   "CDU results lyr a add",
-            //   this._view.map.layers.items.length
-            // );
+
             this._view.whenLayerView(layer).then((layerView) => {
               //console.log("we have the layer view.");
-              //let feature;
               this._view.on("pointer-move", (event) => {
                 this._view.hitTest(event).then((response) => {
                   if (response.results.length) {
@@ -90,51 +82,55 @@ class EsriMap extends React.Component {
                 });
               });
 
-              // this._view.on("click", (event) => {
-              //   console.log("point click");
-              //   if (highlight) {
-              //     highlight.remove();
-              //   }
+              const mapClickHandler = (event) => {
+                //console.log("point click");
+                if (highlight) {
+                  highlight.remove();
+                }
 
-              //   this._view.hitTest(event).then((response) => {
-              //     console.log("point hitTest");
-              //     if (response.results.length) {
-              //       const feature = response.results[0].graphic;
-              //       let { latitude, longitude } = feature.attributes;
-              //       let mapSelectedPlace = feature.attributes.place_of_origin;
+                this._view.hitTest(event).then((response) => {
+                  if (response.results.length) {
+                    const feature = response.results[0].graphic;
+                    let { latitude, longitude } = feature.attributes;
+                    let mapSelectedPlace = feature.attributes.place_of_origin;
 
-              //       this._view.popup.location = {
-              //         latitude: latitude,
-              //         longitude: longitude,
-              //       };
-              //       this._view.popup.title = getFlags(mapSelectedPlace, places);
-              //       this._view.popup.visible = true;
+                    this._view.popup.location = {
+                      latitude: latitude,
+                      longitude: longitude,
+                    };
+                    this._view.popup.title = getFlags(mapSelectedPlace, places);
+                    this._view.popup.visible = true;
 
-              //       if (this.props.selectedPlace === mapSelectedPlace) {
-              //         if (highlight) {
-              //           highlight.remove();
-              //         }
-              //         this.props.removeSelectedPlace();
-              //       } else if (this.props.selectedPlace !== mapSelectedPlace) {
-              //         this.props.selectPlace(mapSelectedPlace);
-              //         highlight = layerView.highlight(feature);
-              //       }
-              //     } else {
-              //       if (this._view.popup.visible) {
-              //         this._view.popup.visible = false;
-              //       }
-              //       if (this.props.selectedPlace !== "") {
-              //         this.props.removeSelectedPlace();
-              //       }
-              //     }
-              //   });
-              // });
+                    if (this.props.selectedPlace === mapSelectedPlace) {
+                      if (highlight) {
+                        highlight.remove();
+                      }
+                      this.props.removeSelectedPlace();
+                    } else if (this.props.selectedPlace !== mapSelectedPlace) {
+                      this.props.selectPlace(mapSelectedPlace);
+                      highlight = layerView.highlight(feature);
+                    }
+                  } else {
+                    if (this._view.popup.visible) {
+                      this._view.popup.visible = false;
+                    }
+                    if (this.props.selectedPlace !== "") {
+                      this.props.removeSelectedPlace();
+                    }
+                  }
+                });
+                // can't remove mapClickListener inside this func, bc disables toggle
+              };
+
+              //works; how to remove?
+              mapClickListener = this._view.on(
+                "immediate-click",
+                mapClickHandler
+              );
+              //end when layerView
             });
-
             //end prevProps.results
           });
-        //end no results else
-        //}
         //end this._view
       }
     }
@@ -143,8 +139,6 @@ class EsriMap extends React.Component {
       if (this._view) {
         const layer = this._view.map.layers.getItemAt(0);
         this._view.whenLayerView(layer).then((layerView) => {
-          //console.log("layer - CDU");
-
           let query = layer.createQuery();
           let queryString = `place_of_origin = '${this.props.selectedPlace}'`;
           query.where = queryString;
@@ -175,7 +169,6 @@ class EsriMap extends React.Component {
             }
             highlight = layerView.highlight(result.features);
           });
-
           //whenLayerView
         });
         //this._view
